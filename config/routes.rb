2c692312479d1443dcd3bd9f48bff51bd172c2ef
kage <81172243+kage95@users.noreply.github.com>
2022-03-03 12:46:37 +0000

    add relationship

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,14 @@ Rails.application.routes.draw do
   get  '/about',   to: 'static_pages#about'
   get  '/contact', to: 'static_pages#contact'
   get  '/signup',  to: 'users#new'
-  resources :users
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
   get "/login", to: "sessions#new"
   post "/login", to: "sessions#create"
   delete "/logout", to: "sessions#destroy"
   resources :microposts, only:[:create,:destroy]
+  resources :relationships,       only: [:create, :destroy]
 end
